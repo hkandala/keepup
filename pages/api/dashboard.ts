41e3a67bd3a62aecdc8c04c9b11e8d9e69046685
Harish Kandala <kandalaharish95@gmail.com>
2022-07-20 21:14:20 +0530

    Add dashboard update API

--- pages/api/dashboard.ts
+++ pages/api/dashboard.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { unstable_getServerSession } from "next-auth/next";
+import Joi from "joi";
 
 import prisma from "../../lib/util/prisma";
 import { Index } from "../../lib/types/index.interface";
@@ -19,7 +20,16 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const session = await unstable_getServerSession(req, res, authOptions);
-  return res.json(await dashboardIndex(session));
+  if (req.method === "GET") {
+    return res.json(await dashboardIndex(session));
+  } else if (req.method === "POST") {
+    if (session) {
+      const [status, data] = await dashboardUpdate(session, req.body);
+      return res.status(status as number).json(data);
+    }
+  } else {
+    return res.status(401).json({ error: "No user available in session." });
+  }
 }
 
 export async function dashboardIndex(session: any) {
@@ -56,6 +66,59 @@ export async function dashboardIndex(session: any) {
       };
     }),
   };
+}
+
+export async function dashboardUpdate(session: any, body: any) {
+  const feedConfigRequestSchema = Joi.object({
+    config: Joi.array()
+      .required()
+      .min(1)
+      .items(
+        Joi.object({
+          id: Joi.string()
+            .required()
+            .valid(...indexFunctionList.map((f) => f().id)),
+          categoryName: Joi.string().allow(null),
+          endpointIndex: indexFunctionList.reduce((joi, f) => {
+            return joi.when("id", {
+              is: f().id,
+              then: Joi.number()
+                .integer()
+                .min(0)
+                .max(f().endpoints.length - 1),
+            });
+          }, Joi.number().required()),
+        })
+      )
+      .unique(
+        (a, b) =>
+          a.id === b.id &&
+          a.categoryName === b.categoryName &&
+          a.endpointIndex === b.endpointIndex
+      ),
+  });
+
+  const { error, value } = feedConfigRequestSchema.validate(body);
+  const requestConfig = value.config as FeedConfig[];
+
+  if (error) {
+    return [400, error];
+  }
+
+  await prisma.feedConfig.upsert({
+    create: {
+      userId: session.user.id,
+      config: (<any>requestConfig) as Prisma.JsonArray,
+    },
+    update: {
+      config: (<any>requestConfig) as Prisma.JsonArray,
+    },
+    where: {
+      userId: session.user.id,
+    },
+  });
+
+  return [200, value];
 }
 
 function fetchIndex(id: string, category: string): Index {
