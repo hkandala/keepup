64257d978cc46e306280514f12f1103e237ce534
Harish Kandala <kandalaharish95@gmail.com>
2022-07-30 18:07:42 +0530

    Update default feed config

--- lib/constants/default-feed-config.ts
+++ lib/constants/default-feed-config.ts
@@ -7,6 +7,11 @@ export default [
     endpointIndex: 0,
   },
   {
+    id: "hashnode",
+    categoryName: null,
+    endpointIndex: 0,
+  },
+  {
     id: "dev",
     categoryName: null,
     endpointIndex: 0,
@@ -22,13 +27,8 @@ export default [
     endpointIndex: 0,
   },
   {
-    id: "reddit",
-    categoryName: "java",
-    endpointIndex: 0,
-  },
-  {
-    id: "reddit",
-    categoryName: "machinelearning",
+    id: "hashnode_tag",
+    categoryName: "javascript",
     endpointIndex: 0,
   },
 ] as FeedConfig[];
