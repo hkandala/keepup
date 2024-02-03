29f9f569ec2b97eec01e9dc154d1ac3142e2d264
Harish Kandala <kandalaharish95@gmail.com>
2024-02-03 03:43:50 +0100

    Add vercel speed insights package

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { CssBaseline } from "@geist-ui/core";
 
 class MyDocument extends Document {
@@ -26,6 +27,7 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
           <Analytics />
+          <SpeedInsights />
         </body>
       </Html>
     );
